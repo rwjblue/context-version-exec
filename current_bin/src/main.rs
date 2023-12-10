8b692026bcf20bb7fefdc3ad976fa590104fdff9
Robert Jackson <me@rwjblue.com>
2023-12-09 21:46:50 -0500

    Add basic CLI parsing to current_bin

--- current_bin/src/main.rs
+++ current_bin/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Specify the manifest version -- 1 uses `prior_bin` and 2+ will use `current_bin`
+    #[arg(long)]
+    manifest_version: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.manifest_version == 1 {
+        println!("prior_bin");
+    } else {
+        println!("current_bin");
+    }
 }
